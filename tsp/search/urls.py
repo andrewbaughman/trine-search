78a00eb1517f5f3651b47d085d1c8a2e7f4a1769
andrewbaughman <andrewbaughman@icloud.com>
2020-12-19 15:03:34 -0500

    Added api and crawler adjustments. I really hope these .pyc files don't break stuff.

--- tsp/search/urls.py
+++ tsp/search/urls.py
@@ -1,8 +1,19 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 
-from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
+from search import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('results/<str:query>/', views.results, name='results'),
+	path('', views.index, name='index'),
+	path('search/admin/', admin.site.urls),
+	path('results/<str:query>/', views.results, name='results'),
+	path('api/', views.PageList.as_view()),
+	path('api/<int:pk>/', views.PageDetail.as_view()),
+	path('users/', views.UserList.as_view()),
+	path('users/<int:pk>/', views.UserDetail.as_view()),
+	path('api-auth/', include('rest_framework.urls')),
+	path('add_page/', views.AddPage.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- tsp/search/views.py
+++ tsp/search/views.py
@@ -1,9 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+
+from rest_framework import generics
+from .models import page
+from .serializers import PageSerializer, UserSerializer
+from django.forms.models import model_to_dict
+
+from django.views import View
 
 def index(request):
-    return render(request, 'home.html')
+	return render(request, 'home.html')
 
 def results(request, query):
 	results = searchAlgorithm(query)
@@ -20,3 +29,39 @@ def searchAlgorithm(query):
 		results.append(result)
 
 	return results
+
+class AddPage(View):
+	def post(self, request):
+		ret = {}
+
+		url = request.POST.get('url')
+		title = request.POST.get('title')
+		description = request.POST.get('description')
+		
+		webpage = page.objects.create(url=url, title=title, description=description)
+
+		ret['page'] = model_to_dict(webpage)
+
+		return JsonResponse(ret)
+
+
+
+class PageList(generics.ListCreateAPIView):
+	queryset = page.objects.all()
+	serializer_class = PageSerializer
+	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class PageDetail(generics.RetrieveUpdateDestroyAPIView):
+	queryset = page.objects.all()
+	serializer_class = PageSerializer
+	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+class UserList(generics.ListAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
