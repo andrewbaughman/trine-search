a523f8130c0d140384bec9eb2cc84585ec3de032
andrewbaughman <andrewbaughman@icloud.com>
2021-02-01 09:42:03 -0500

    Replaced manual input check in button click with function call. Removed old search function

--- tsp/static/js/script.js
+++ tsp/static/js/script.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
     $('#search_button').click(function() {
-		if ($('.query').val() == '') {
-			window.location = "/";
-		} else {
-			window.location = "/results/"+ $('.query').val() +"/";
-		}
+		search($('.query').val());
     });
 });
 
@@ -22,30 +18,4 @@ function search(query) {
 	} else {
 		window.location = "/results/"+ query +"/";
 	}
-}
-
-
-//No longer used, but left here as an example.
-function search(query){
-    $.ajax({
-        type: 'GET',
-        data: {
-            'query': query,
-        },
-        url: '/search/',
-        success: function(response) {
-			//This is what the response actually looks like.
-			console.log(response);
-			$(document).find('.results').html("");
-			$.each(response['results'], function(index, result){
-				$(document).find('.results').html($(document).find('.results').html()
-				+"<div class='result'>"
-				+	"<tr>"
-				+		"<url><td>"+ result['url'] +"</td></url>"
-				+		"<td><a href= "+ result['url'] +"><title-result></br>"+ result['title'] +"</td></title-result></a>"
-				+		"<td></br><info><td>"+ result['description'] +"</td></info>"
-				+"</div>");
-			});
-        }
-    });
 }
