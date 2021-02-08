20b14d1280da9facd5e758bb66d9a5cd230ed5c5
andrewbaughman <andrewbaughman@icloud.com>
2021-02-08 12:13:26 -0500

    Added tables

--- tsp/search/models.py
+++ tsp/search/models.py
@@ -7,3 +7,22 @@ class page(models.Model):
 	url = models.URLField(max_length=200)
 	title = models.CharField(max_length=50)
 	description = models.TextField(max_length=400)
+
+
+class keywords(models.Model):
+	url = models.ForeignKey('links', on_delete=models.CASCADE)
+	keyword = models.CharField(max_length=50)
+	times_on_page = models.IntegerField()
+
+
+class links(models.Model):
+	destination = models.URLField(max_length=200, primary_key=True)
+	source = models.URLField(max_length=200)
+	isTrine = models.BooleanField()
+	visited = models.BooleanField()
+
+
+class page_results(models.Model):
+	url = models.ForeignKey('links', on_delete=models.CASCADE)
+	title = models.CharField(max_length=50)
+	description = models.CharField(max_length=250)
