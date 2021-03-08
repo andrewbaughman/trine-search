0e040a695748f252d692fbc1c5b28d5e4923046d
ninjajosh5 <ninjajosh5@gmail.com>
2021-03-08 16:21:00 -0500

    added theme modifier

--- tsp/static/js/script.js
+++ tsp/static/js/script.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
-	/*		load theme from cache, default dark		*/
-	defaultTheme();
 	/*		load toggle from storage				*/
 	defaultToggle();
 	/*			toggle theme on click				*/
+	/*
 	$('.btn-toggle').on('click', function () {
 		var currentTheme = localStorage.getItem("theme");
 		if (currentTheme == 'light') {
@@ -14,6 +13,7 @@ $(document).ready(function () {
 			localStorage.setItem("theme", "light");
 		}
 	});
+	*/
 	//$('.query').attr('isTrine', 'false');
 	$('.trine-toggle').on('change', function () {
 		//var current = $('#homeInpput').attr('placeholder');
@@ -41,7 +41,6 @@ $(document).keypress(function (event) {
 });
 
 function search(query, isTrine) {
-	var toggleSetting = localStorage.getItem("toggle"); //for later
 	if (query == '') {
 		window.location = "/";
 	} else if (isTrine == 'false') {
@@ -65,49 +64,4 @@ function defaultToggle() {
 	}
 }
 
-function defaultTheme() {
-	var currentToggle = localStorage.getItem("theme");
-	if (currentToggle == "light") { //change this parameter to set default
-		setlight(); //
-	} else {
-		setdark();
-	}
-}
 
-/*			These are theme elements			*/
-
-function setlight() {
-	var root = document.documentElement;
-	root.style.setProperty('--main-bg-color', 'lightblue');
-	root.style.setProperty('--toggle-button-color', 'white');
-	root.style.setProperty('--toggle-button-text-color', 'black');
-	root.style.setProperty('--second-bg-color', 'white');
-	root.style.setProperty('--highlight-color', '#9cf');
-	root.style.setProperty('--search-bar', 'white')
-	root.style.setProperty('--main-text', 'black')
-	root.style.setProperty('--info-text', '#4d5156')
-	root.style.setProperty('--url-text', '#202124')
-	root.style.setProperty('--title-text', 'black')
-	root.style.setProperty('--title-text-hover', 'blue')
-	root.style.setProperty('--title-text-visit', 'purple')
-	root.style.setProperty('--padding-color', '#f1f5ff')
-	$('.btn-toggle').html('light');
-}
-
-function setdark() {
-	var root = document.documentElement;
-	root.style.setProperty('--main-bg-color', '#36393f');
-	root.style.setProperty('--toggle-button-color', '#1a1a1a');
-	root.style.setProperty('--toggle-button-text-color', 'white');
-	root.style.setProperty('--second-bg-color', '#36393f');
-	root.style.setProperty('--highlight-color', '#737373');
-	root.style.setProperty('--search-bar', '#1a1a1a')
-	root.style.setProperty('--main-text', 'white')
-	root.style.setProperty('--info-text', '#b4b6b9')
-	root.style.setProperty('--url-text', 'white')
-	root.style.setProperty('--title-text', 'white')
-	root.style.setProperty('--title-text-hover', 'lightblue')
-	root.style.setProperty('--title-text-visit', '#ffb9fe')
-	root.style.setProperty('--padding-color', '#2a3547')
-	$('.btn-toggle').html('dark');
-}
