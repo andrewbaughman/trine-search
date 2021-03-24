12909f48c9b48cc7154e70cb333b823887cad4f6
josh-trine <joshuamiddleton87@gmail.com>
2021-03-24 17:47:11 -0400

    changes

--- tsp/search/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import *
-admin.site.register(links)
-admin.site.register(keywords)
-admin.site.register(page)
-admin.site.register(edges)
