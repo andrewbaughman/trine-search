87434b4a6196397fee594d1c50d826eedcf267f0
josh-trine <joshuamiddleton87@gmail.com>
2021-04-18 18:40:26 -0400

    adds quoting

--- search/views.py
+++ search/views.py
@@ -36,6 +36,20 @@ def results(request):
 	lucky = (request.GET.get('lucky')=='True')
 	random = (request.GET.get('random')=='True')
 	query = init_query.lower().split()
+	start_quoting = False
+	index = 0
+	for word in query:
+		if word[0] == '"' and word[-1] != '"':
+			query[index] = word + '"'
+			start_quoting = True
+		elif word[0] != '"' and word[-1] == '"':
+			query[index] = '"' + word
+			start_quoting = False
+		elif word[0] == '"' and word[-1] == '"':
+			pass
+		elif start_quoting:
+			query[index] = '"' + word + '"'
+		index += 1
 	query = parse_query(query)
 	#get a ranked list of Trine pages based on keyword and important words
 	if not random: 
@@ -92,7 +106,6 @@ def get_random_page():
 		return list()
 	while True:
 		random_id = randint(0, maximum)
-		print(random_id)
 		results = []
 		try:
 			site = links.objects.get(id=random_id)
@@ -171,6 +184,10 @@ def get_ranked_list(entity_list, isTrine):
 	#cycle through search terms
 	urls_to_keyword = False
 	for entity in entity_list:
+		exact = False
+		if entity[0] == '"':
+			entity = entity[1:-1]
+			exact = True
 		#a dictionary the has 'key' and values that are 2-dimensional- one for word freqency
 		# and the other for the number of important words that match the query
 		#remove the last 's' of a word
@@ -180,14 +197,22 @@ def get_ranked_list(entity_list, isTrine):
 		try:
 			#get keyword objects
 			if first_time:
-				if isTrine:
+				if isTrine and exact:
+					urls_to_keyword = keywords.objects.filter(keyword=entity ,url__isTrine=1)
+				elif isTrine:
 					urls_to_keyword = keywords.objects.filter(Q(keyword=entity) | Q(keyword=(entity + 's')) ,url__isTrine=1)
+				elif exact:
+					urls_to_keyword = keywords.objects.filter(keyword=entity)
 				else:
 					urls_to_keyword = keywords.objects.filter(Q(keyword=entity) | Q(keyword=(entity + 's')))
 				first_time = False
 			else:
-				if isTrine:
+				if isTrine and exact:
+					kwobjects = keywords.objects.filter(keyword=entity ,url__isTrine=1)
+				elif isTrine:
 					kwobjects = keywords.objects.filter(Q(keyword=entity) | Q(keyword=(entity + 's')) ,url__isTrine=1)
+				elif exact:
+					kwobjects = keywords.objects.filter(eyword=entity)
 				else:
 					kwobjects = keywords.objects.filter(Q(keyword=entity) | Q(keyword=(entity + 's')))
 				urls_to_keyword = (urls_to_keyword | kwobjects)
